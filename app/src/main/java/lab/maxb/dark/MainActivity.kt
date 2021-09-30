347edb9fd886a0706ea41150c5c4548db7ac9c0f
Бабышев Максим <cmdazaza@gmail.com>
2021-09-30 16:40:27 +0300

    + Toolbar

--- app/src/main/java/lab/maxb/dark/MainActivity.kt
+++ app/src/main/java/lab/maxb/dark/MainActivity.kt
@@ -4,16 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.coroutines.launch
 import lab.maxb.dark.Domain.Model.User
 import lab.maxb.dark.Presentation.Repository.Repository
+import lab.maxb.dark.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mBinding: MainActivityBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mBinding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(mBinding!!.root)
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
-        setContentView(R.layout.main_activity)
+
+        setSupportActionBar(mBinding!!.toolbar)
+        val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+
         Repository.init(application)
 
         // Hardcode current user
