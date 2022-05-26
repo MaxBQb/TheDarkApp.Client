6ef5a5ec4a5bcb20d56fdf077410ac25d0bf4f18
Бабышев Максим <cmdazaza@gmail.com>
2022-05-26 17:40:28 +0300

    Wrap service in network exceptions handlers

--- app/src/main/java/lab/maxb/dark/di/FlexibleDIModule.kt
+++ app/src/main/java/lab/maxb/dark/di/FlexibleDIModule.kt
@@ -1,9 +1,12 @@
 package lab.maxb.dark.di
 
+import lab.maxb.dark.presentation.repository.network.dark.DarkService
+import lab.maxb.dark.presentation.repository.network.dark.DarkServiceImpl
 import lab.maxb.dark.presentation.repository.room.LocalDatabase
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 internal val flexibleDIModule = module {
     single { LocalDatabase.build(androidApplication()) }
+    single<DarkService> { DarkServiceImpl(get()) }
 }
