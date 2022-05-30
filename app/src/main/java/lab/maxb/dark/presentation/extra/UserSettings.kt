cc2d264cb69d265025457b419c05778bc5869173
Бабышев Максим <cmdazaza@gmail.com>
2022-05-30 15:19:51 +0300

    Use hidden master key instead of default one

--- app/src/main/java/lab/maxb/dark/presentation/extra/UserSettings.kt
+++ app/src/main/java/lab/maxb/dark/presentation/extra/UserSettings.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
+import lab.maxb.dark.BuildConfig
 import lab.maxb.dark.presentation.extra.delegates.property
 import org.koin.core.annotation.Single
 
@@ -13,8 +14,8 @@ class UserSettings(context: Context) {
     private val pref = PreferenceManager.getDefaultSharedPreferences(context)
     private val securePref: SharedPreferences = EncryptedSharedPreferences.create(
         context,
-        "secure_dark_preferences",
-        MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build(),
+        "secure_dark.preferences",
+        MasterKey.Builder(context, BuildConfig.SECURE_PREFS_MASTER_KEY).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build(),
         EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
         EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
     )
