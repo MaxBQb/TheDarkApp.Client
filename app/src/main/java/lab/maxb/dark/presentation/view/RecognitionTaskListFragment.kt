c4ecb207aff08cd2dfebfd5c3712a87583f51596
Бабышев Максим <cmdazaza@gmail.com>
2022-09-18 00:26:31 +0300

    Switch recognition tasks list fragment to JC

--- app/src/main/java/lab/maxb/dark/presentation/view/RecognitionTaskListFragment.kt
+++ app/src/main/java/lab/maxb/dark/presentation/view/RecognitionTaskListFragment.kt
@@ -1,66 +1,218 @@
 package lab.maxb.dark.presentation.view
 
-import android.graphics.drawable.Animatable
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.view.View
-import androidx.core.view.isVisible
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
+import com.bumptech.glide.load.model.GlideUrl
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.animation.crossfade.CrossfadePlugin
+import com.skydoves.landscapist.components.rememberImageComponent
+import com.skydoves.landscapist.glide.GlideImage
 import lab.maxb.dark.R
-import lab.maxb.dark.databinding.RecognitionTaskListFragmentBinding
 import lab.maxb.dark.domain.model.RecognitionTask
-import lab.maxb.dark.presentation.extra.GlideApp
-import lab.maxb.dark.presentation.extra.delegates.autoCleaned
-import lab.maxb.dark.presentation.extra.delegates.viewBinding
 import lab.maxb.dark.presentation.extra.navigate
-import lab.maxb.dark.presentation.extra.observe
-import lab.maxb.dark.presentation.view.adapter.RecognitionTaskListAdapter
 import lab.maxb.dark.presentation.viewModel.RecognitionTaskListViewModel
+import lab.maxb.dark.ui.theme.DarkAppTheme
+import lab.maxb.dark.ui.theme.units.sdp
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 
-class RecognitionTaskListFragment : Fragment(R.layout.recognition_task_list_fragment) {
+class RecognitionTaskListFragment : Fragment() {
     private val mViewModel: RecognitionTaskListViewModel by sharedViewModel()
-    private val mBinding: RecognitionTaskListFragmentBinding by viewBinding()
-    private var mAdapter: RecognitionTaskListAdapter by autoCleaned()
-    private var mPlaceholder: Drawable by autoCleaned()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        mViewModel.isTaskCreationAllowed observe {
-            mBinding.fab.isVisible = it
-        }
-        mBinding.fab.setOnClickListener {
-            RecognitionTaskListFragmentDirections.navToAddTaskFragment().navigate()
-        }
-        mPlaceholder = AnimatedVectorDrawableCompat.create(
-            requireContext(),
-            R.drawable.loading_vector
-        )!!
-        with (mPlaceholder as Animatable) {
-            stop()
-            start()
-        }
-        mAdapter = RecognitionTaskListAdapter(GlideApp.with(this),
-            !mViewModel.isTaskCreationAllowed.value
-        ) {
-            load(mViewModel.getImage(it))
-                .transition(withCrossFade())
-                .placeholder(mPlaceholder)
-                .error(R.drawable.ic_error)
-        }
-        mBinding.recognitionTaskListRecycler.adapter = mAdapter
-        mBinding.recognitionTaskListRecycler.addOnScrollListener(mAdapter.preloader.raw)
-        mAdapter.onElementClickListener = { _: View, task: RecognitionTask ->
-            RecognitionTaskListFragmentDirections.navToSolveTaskFragment(
-                task.id
-            ).navigate()
-        }
-
-        mViewModel.recognitionTaskList observe {
-            mAdapter.submitData(it)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        setContent {
+            RecognitionTaskListRoot(
+                mViewModel,
+                onItemClick = {
+                    RecognitionTaskListFragmentDirections
+                        .navToSolveTaskFragment(it.id).navigate()
+                },
+                onFabClick = {
+                    RecognitionTaskListFragmentDirections
+                        .navToAddTaskFragment().navigate()
+                }
+            )
         }
     }
 }
+
+@Composable
+fun RecognitionTaskListRoot(
+    viewModel: RecognitionTaskListViewModel,
+    onItemClick: (RecognitionTask) -> Unit,
+    onFabClick: () -> Unit,
+) = DarkAppTheme {
+    val items = viewModel.recognitionTaskList.collectAsLazyPagingItems()
+    val isTaskCreationAllowed by viewModel.isTaskCreationAllowed.collectAsState()
+
+    Surface {
+        RecognitionTaskList(
+            items,
+            onItemClick = onItemClick,
+            resolveImage = viewModel::getImage
+        )
+        if (isTaskCreationAllowed)
+            FloatingActionButton(
+                onClick = onFabClick,
+                shape = CircleShape,
+                containerColor = colorScheme.secondaryContainer,
+                modifier = Modifier.padding(32.sdp)
+                    .wrapContentSize(Alignment.BottomEnd)
+            ) {
+                Image(painterResource(id = R.drawable.ic_plus), null)
+            }
+    }
+}
+
+@Composable
+private fun RecognitionTaskList(
+    items: LazyPagingItems<RecognitionTask>,
+    onItemClick: (RecognitionTask) -> Unit,
+    resolveImage: (String) -> GlideUrl
+) {
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(0.dp, 8.sdp)
+    ) {
+        items(items = items, itemContent = { item ->
+            item?.let {
+                RecognitionTaskCard(
+                    it,
+                    onClick = { onItemClick(it) },
+                    resolveImage = resolveImage
+                )
+            } ?: run {
+                LoadingCircle(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(16.sdp, 8.sdp)
+                        .wrapContentWidth(Alignment.CenterHorizontally),
+                    5
+                )
+            }
+        })
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RecognitionTaskCard(
+    item: RecognitionTask,
+    onClick: () -> Unit,
+    resolveImage: (String) -> GlideUrl
+) {
+    val taskOwnerName = item.owner?.name ?: ""
+    val elevation = if (item.reviewed) 300 else 500
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.sdp)
+            .padding(16.sdp, 8.sdp),
+        tonalElevation = elevation.dp,
+        shape = RoundedCornerShape(16.dp),
+        onClick = onClick
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            GlideImage(
+                resolveImage(item.images?.firstOrNull() ?: ""),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.sdp),
+                imageOptions = ImageOptions(contentScale = ContentScale.Inside),
+                failure = {
+                    LoadingError(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(16.sdp))
+                },
+                loading = {
+                    Box(
+                        Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        LoadingCircle(0.7f)
+                    }
+                },
+                component = rememberImageComponent {
+                    +CrossfadePlugin(duration = 1500)
+                }
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                colorScheme.surfaceColorAtElevation(elevation.dp),
+                                Color.Transparent
+                            ),
+                            endY = 100f
+                        )
+                    )
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.sdp),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Text(taskOwnerName)
+            }
+        }
+    }
+}
+
+@Composable
+fun LoadingError(modifier: Modifier = Modifier) {
+    Image(
+        painterResource(R.drawable.ic_error),
+        null,
+        modifier
+    )
+}
+
+@Composable
+fun LoadingCircle(modifier: Modifier = Modifier, width: Int = 5) {
+    CircularProgressIndicator(
+        modifier,
+        color = colorScheme.onSurface.copy(0.1f),
+        strokeWidth = width.sdp
+    )
+}
+
+@Composable
+fun LoadingCircle(size: Float, modifier: Modifier = Modifier, width: Int = 5)
+    = LoadingCircle(modifier.fillMaxSize(size/2f), width)
