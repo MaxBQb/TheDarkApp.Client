3e450e348015191553c798c52c33c8a4968d0ba2
Бабышев Максим <cmdazaza@gmail.com>
2022-09-19 16:03:52 +0300

    Update DI

--- app/src/main/java/lab/maxb/dark/DarkApplication.kt
+++ app/src/main/java/lab/maxb/dark/DarkApplication.kt
@@ -8,7 +8,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
-import org.koin.ksp.generated.module
+import org.koin.ksp.generated.*
 
 
 @Suppress("unused") // Used by AndroidManifest.xml
