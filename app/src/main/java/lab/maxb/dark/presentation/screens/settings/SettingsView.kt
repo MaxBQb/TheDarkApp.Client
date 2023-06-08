c4fd07f915d7755e150d103cae3608538420a81f
Бабышев Максим <cmdazaza@gmail.com>
2023-06-08 22:43:32 +0300

    + Version info in settings

--- app/src/main/java/lab/maxb/dark/presentation/screens/settings/SettingsView.kt
+++ app/src/main/java/lab/maxb/dark/presentation/screens/settings/SettingsView.kt
@@ -1,8 +1,13 @@
 package lab.maxb.dark.presentation.screens.settings
 
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -10,13 +15,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.core.os.LocaleListCompat
 import androidx.navigation.NavController
 import com.alorma.compose.settings.ui.SettingsMenuLink
 import com.ramcosta.composedestinations.annotation.Destination
 import com.vanpra.composematerialdialogs.rememberMaterialDialogState
+import lab.maxb.dark.BuildConfig
 import lab.maxb.dark.R
 import lab.maxb.dark.presentation.components.ChooseLocaleDialog
 import lab.maxb.dark.presentation.components.NavBackIcon
@@ -25,6 +34,7 @@ import lab.maxb.dark.presentation.components.TopBar
 import lab.maxb.dark.presentation.components.getLanguageCode
 import lab.maxb.dark.presentation.components.getLanguageName
 import lab.maxb.dark.presentation.extra.ChangedEffect
+import lab.maxb.dark.ui.theme.spacing
 import org.koin.androidx.compose.getViewModel
 
 
@@ -63,22 +73,45 @@ fun SettingsRootStateless(
 ) {
     val chooseLocaleDialogState = rememberMaterialDialogState()
     val currentLanguage = getLanguageCode()
-
-    Column(Modifier.fillMaxSize()) {
-        SettingsMenuLink(
-            icon = { Icon(painterResource(R.drawable.ic_language), null) },
-            title = { Text(stringResource(R.string.settings_language_title)) },
-            subtitle = {
-                Text(
-                    text = if (uiState.locale.isEmpty()) stringResource(
-                        R.string.settings_language_auto,
-                        currentLanguage
-                    ) else getLanguageName(),
-                    style = MaterialTheme.typography.bodyMedium,
-                )
+    Column(
+        Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            SettingsMenuLink(
+                icon = { Icon(painterResource(R.drawable.ic_language), null) },
+                title = { Text(stringResource(R.string.settings_language_title)) },
+                subtitle = {
+                    Text(
+                        text = if (uiState.locale.isEmpty()) stringResource(
+                            R.string.settings_language_auto,
+                            currentLanguage
+                        ) else getLanguageName(),
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                },
+                onClick = { chooseLocaleDialogState.show() },
+            )
+        }
+        val uriHandler = LocalUriHandler.current
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth().clickable {
+                uriHandler.openUri(BuildConfig.APP_REPOSITORY)
             },
-            onClick = { chooseLocaleDialogState.show() },
-        )
+        ) {
+            Text(
+                text = stringResource(
+                    R.string.settings_version_info,
+                    stringResource(R.string.app_name),
+                    BuildConfig.VERSION_NAME
+                ),
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSecondary,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(MaterialTheme.spacing.normal),
+            )
+        }
     }
     ChooseLocaleDialog(
         chooseLocaleDialogState,
