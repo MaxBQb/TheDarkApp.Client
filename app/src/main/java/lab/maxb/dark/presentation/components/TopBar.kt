55a072cf532627e704a496659dac2b6f1c2f8a05
Бабышев Максим <cmdazaza@gmail.com>
2023-06-10 14:27:12 +0300

    Fix: Titles wraps in topbar

--- app/src/main/java/lab/maxb/dark/presentation/components/TopBar.kt
+++ app/src/main/java/lab/maxb/dark/presentation/components/TopBar.kt
@@ -1,12 +1,15 @@
 package lab.maxb.dark.presentation.components
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,10 +20,13 @@ fun TopBar(
 ) {
     TopAppBar(
         title = {
+            val scroll = rememberScrollState()
             Text(
                 text = title,
                 color = MaterialTheme.colorScheme.onPrimaryContainer,
                 style = MaterialTheme.typography.titleLarge,
+                softWrap = false,
+                modifier = Modifier.horizontalScroll(scroll)
             )
         },
         navigationIcon = navigationIcon,
