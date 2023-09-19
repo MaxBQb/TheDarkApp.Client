026d63e4637c4bc023baeea984615990d94bb8fe
Бабышев Максим <cmdazaza@gmail.com>
2023-09-19 19:51:47 +0300

    Fix: domain.repo depends on data.datasource

--- app/src/main/java/lab/maxb/dark/data/repository/SettingsRepositoryImpl.kt
+++ app/src/main/java/lab/maxb/dark/data/repository/SettingsRepositoryImpl.kt
@@ -1,12 +1,15 @@
 package lab.maxb.dark.data.repository
 
 import lab.maxb.dark.data.datasource.local.SettingsLocalDataSource
+import lab.maxb.dark.domain.model.Settings
 import lab.maxb.dark.domain.repository.SettingsRepository
 import org.koin.core.annotation.Singleton
 
-@Singleton([SettingsRepository::class])
+@Singleton
 class SettingsRepositoryImpl(
     private val localDataSource: SettingsLocalDataSource,
-) : SettingsRepository, SettingsLocalDataSource by localDataSource {
+) : SettingsRepository {
     override val settings by localDataSource::data
+    override suspend fun update(transform: suspend (Settings) -> Settings)
+        = localDataSource.update(transform)
 }
--- app/src/main/java/lab/maxb/dark/domain/repository/SettingsRepository.kt
+++ app/src/main/java/lab/maxb/dark/domain/repository/SettingsRepository.kt
@@ -1,9 +1,9 @@
 package lab.maxb.dark.domain.repository
 
 import kotlinx.coroutines.flow.Flow
-import lab.maxb.dark.data.datasource.local.SettingsLocalDataSource
 import lab.maxb.dark.domain.model.Settings
 
-interface SettingsRepository: SettingsLocalDataSource {
+interface SettingsRepository {
     val settings: Flow<Settings>
+    suspend fun update(transform: suspend (Settings) -> Settings)
 }
