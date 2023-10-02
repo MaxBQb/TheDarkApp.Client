9bb260fe621902210224187a874180c353f796d0
Бабышев Максим <cmdazaza@gmail.com>
2023-10-02 23:38:14 +0300

    Refactoring: DI declaration now composed, instead of being listed
    Stage #5

--- app/src/main/java/lab/maxb/dark/ui/AppModule.kt
+++ app/src/main/java/lab/maxb/dark/ui/AppModule.kt
@@ -1,8 +1,82 @@
 package lab.maxb.dark.ui
 
+import lab.maxb.dark.data.CoreDataModule
+import lab.maxb.dark.data.FeatureArticlesDataModule
+import lab.maxb.dark.data.FeatureAuthDataModule
+import lab.maxb.dark.data.FeatureDataModule
+import lab.maxb.dark.data.FeaturePagingDataModule
+import lab.maxb.dark.data.FeatureSettingsDataModule
+import lab.maxb.dark.data.FeatureTasksDataModule
+import lab.maxb.dark.data.FeatureUsersDataModule
+import lab.maxb.dark.domain.FeatureArticlesDomainModule
+import lab.maxb.dark.domain.FeatureAuthDomainModule
+import lab.maxb.dark.domain.FeatureSettingsDomainModule
+import lab.maxb.dark.domain.FeatureTasksDomainModule
+import lab.maxb.dark.domain.FeatureUsersDomainModule
 import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Module
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import org.koin.core.annotation.Module as AModule
+import org.koin.ksp.generated.*
 
-@Module
+@AModule
 @ComponentScan("lab.maxb.dark")
 class AppModule
+
+internal val allModules get() = module {
+    includesFeatures()
+    includes(
+        AppModule().module,
+        CoreDataModule().module,
+    )
+}
+
+private fun Module.includesFeatures() {
+    includesArticles()
+    includesUsers()
+    includesSettings()
+    includesAuth()
+    includesTasks()
+    includes(
+        FeatureDataModule().module,
+        FeatureNavigationUIModule().module,
+        FeaturePagingDataModule().module,
+        FeatureNavigationDrawerUIModule().module,
+    )
+}
+
+private fun Module.includesArticles() = includes(
+    FeatureArticlesDomainModule().module,
+    FeatureArticlesDataModule().module,
+    FeatureArticlesUIModule().module,
+)
+
+private fun Module.includesUsers() = includes(
+    FeatureUsersDomainModule().module,
+    FeatureUsersDataModule().module,
+    FeatureWelcomeUIModule().module,
+)
+
+private fun Module.includesSettings() = includes(
+    FeatureSettingsDomainModule().module,
+    FeatureSettingsDataModule().module,
+    FeatureSettingsUIModule().module,
+)
+
+private fun Module.includesAuth() = includes(
+    FeatureAuthDomainModule().module,
+    FeatureAuthDataModule().module,
+    FeatureAuthUIModule().module,
+)
+private fun Module.includesTasks() {
+    includesTasksUI()
+    includes(
+        FeatureTasksDomainModule().module,
+        FeatureTasksDataModule().module,
+    )
+}
+private fun Module.includesTasksUI() = includes(
+    FeatureAddTaskUIModule().module,
+    FeatureSolveTaskUIModule().module,
+    FeatureTasksListUIModule().module,
+)
--- app/src/main/java/lab/maxb/dark/ui/DarkApplication.kt
+++ app/src/main/java/lab/maxb/dark/ui/DarkApplication.kt
@@ -2,24 +2,10 @@ package lab.maxb.dark.ui
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
-import lab.maxb.dark.data.CoreDataModule
-import lab.maxb.dark.data.FeatureArticlesDataModule
-import lab.maxb.dark.data.FeatureAuthDataModule
-import lab.maxb.dark.data.FeatureDataModule
-import lab.maxb.dark.data.FeaturePagingDataModule
-import lab.maxb.dark.data.FeatureSettingsDataModule
-import lab.maxb.dark.data.FeatureTasksDataModule
-import lab.maxb.dark.data.FeatureUsersDataModule
-import lab.maxb.dark.domain.FeatureArticlesDomainModule
-import lab.maxb.dark.domain.FeatureAuthDomainModule
-import lab.maxb.dark.domain.FeatureSettingsDomainModule
-import lab.maxb.dark.domain.FeatureTasksDomainModule
-import lab.maxb.dark.domain.FeatureUsersDomainModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
-import org.koin.ksp.generated.module
 
 
 class DarkApplication : Application() {
@@ -29,22 +15,7 @@ class DarkApplication : Application() {
         startKoin {
             androidLogger(Level.ERROR)
             androidContext(this@DarkApplication)
-            modules(
-                AppModule().module,
-                FeatureDataModule().module,
-                CoreDataModule().module,
-                FeatureNavigationUIModule().module,
-                FeatureArticlesDomainModule().module, FeatureArticlesDataModule().module, FeatureArticlesUIModule().module,
-                FeatureUsersDomainModule().module, FeatureUsersDataModule().module, FeatureWelcomeUIModule().module,
-                FeatureSettingsDomainModule().module, FeatureSettingsDataModule().module, FeatureSettingsUIModule().module,
-                FeatureAuthDomainModule().module, FeatureAuthDataModule().module, FeatureAuthUIModule().module,
-                FeatureTasksDomainModule().module, FeatureTasksDataModule().module,
-                FeatureAddTaskUIModule().module,
-                FeatureSolveTaskUIModule().module,
-                FeatureTasksListUIModule().module,
-                FeaturePagingDataModule().module,
-                FeatureNavigationDrawerUIModule().module,
-            )
+            modules(allModules)
         }
     }
 }
